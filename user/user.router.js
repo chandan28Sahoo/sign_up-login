fd67acc885582e4611db6adcc8fcbf65f437f004
Chandan SAHOO <64142051+chandan28Sahoo@users.noreply.github.com>
2020-10-21 19:41:17 +0530

    Update user.router.js

--- user/user.router.js
+++ user/user.router.js
@@ -4,46 +4,46 @@ router.post('/',creatUser);
 module.exports=router;
 
 
-// module.exports = function(router,jwt){
-//     router.post('/sign',(req,res)=>{
-//         let user = {
-//             "firstName":"vishal",
-//             "lastName":"kumar",
-//             "gender":"male",
-//             "email":"vishal@gmail.com",
-//             "password":"12345"
-//         }
-//         let token = jwt.sign({email:user.email,password:user.password},'secretkey')
-//         console.log(token);
-//         importdata('registration').insert(user)
-//         .then((result)=>{
-//             console.log(result);
-//             if(result){
-//                 res.send("your account has been created succersfully")
-//             }else{
-//                 res.send("somthing error \n please try again")
-//             }
-//         }).catch((err)=>{console.log(err);})
+module.exports = function(router,jwt){
+    router.post('/sign',(req,res)=>{
+        let user = {
+            "firstName":"vishal",
+            "lastName":"kumar",
+            "gender":"male",
+            "email":"vishal@gmail.com",
+            "password":"12345"
+        }
+        let token = jwt.sign({email:user.email,password:user.password},'secretkey')
+        console.log(token);
+        importdata('registration').insert(user)
+        .then((result)=>{
+            console.log(result);
+            if(result){
+                res.send("your account has been created succersfully")
+            }else{
+                res.send("somthing error \n please try again")
+            }
+        }).catch((err)=>{console.log(err);})
             
         
-//     });
+    });
 
-//     router.post('/login',(req,res)=>{
-//         let user = {
-//             email:"vishl@gmail.com",
-//             password:"12345"
-//         }
-//         let decode = jwt.verify(req.headers.authorization,'secretkey')
-//         if(user.email == decode.email){
-//             if(user.password == decode.password){
-//                 res.send("login succesfully")
-//             }else{
-//                 res.send("Invalid password")
-//                 // console.log("Invalid password");
-//             }
-//         }else{
-//             res.send("Invalid mail")
-//             // console.log("Invalid mail");
-//         }
-//     })
-// }
+    router.post('/login',(req,res)=>{
+        let user = {
+            email:"vishl@gmail.com",
+            password:"12345"
+        }
+        let decode = jwt.verify(req.headers.authorization,'secretkey')
+        if(user.email == decode.email){
+            if(user.password == decode.password){
+                res.send("login succesfully")
+            }else{
+                res.send("Invalid password")
+                // console.log("Invalid password");
+            }
+        }else{
+            res.send("Invalid mail")
+            // console.log("Invalid mail");
+        }
+    })
+}
